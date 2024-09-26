a50e08d0e2d76f14b9d8e7fa11bf734ee8a2a58a
aizazhalima1 <halimaaizaz@hotmail.com>
2024-09-26 10:37:12 -0400

    Implement node

--- server.js
+++ server.js
@@ -1,62 +1,66 @@
 const http = require('http');
-const fs = require('fs')
+const fs = require('fs');
 const url = require('url');
 const querystring = require('querystring');
-const figlet = require('figlet')
+const figlet = require('figlet');
 
 const server = http.createServer((req, res) => {
   const page = url.parse(req.url).pathname;
   const params = querystring.parse(url.parse(req.url).query);
   console.log(page);
-  
-  if (page == '/') {
-    fs.readFile('index.html', function(err, data) {
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(data);
-      res.end();
-    });
-  }
 
-  else if (page == '/api') {
-   
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        let arr=['Rock','Paper','Scissor']
-        let rand=Math.floor(Math.random() * 3)
-        let valComputer=arr[rand]
-        const objToJson = {
-          computerVal: valComputer
-        }
-        
-        res.end(JSON.stringify(objToJson));
-      }
-  else if (page == '/css/style.css'){
-    fs.readFile('css/style.css', function(err, data) {
-
-      res.write(data);
-      res.end();
-    }
-    
-  );
-  }
-  else if (page == '/js/main.js'){
-    fs.readFile('js/main.js', function(err, data) {
-      res.writeHead(200, {'Content-Type': 'text/javascript'});
-      res.write(data);
-      res.end();
-    });
-  }else{
-    figlet('404!!', function(err, data) {
+  if (page === '/') {
+    fs.readFile('index.html', (err, data) => {
       if (err) {
-          console.log('Something went wrong...');
-          console.dir(err);
-          return;
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Server error');
+        return;
       }
-      res.write(data);
-      res.end();
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(data);
+    });
+  } else if (page === '/api') {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    const arr = ['Rock', 'Paper', 'Scissor'];
+    const rand = Math.floor(Math.random() * arr.length);
+    const valComputer = arr[rand];
+    const objToJson = { computerVal: valComputer };
+    res.end(JSON.stringify(objToJson));
+  } else if (page === '/css/style.css') {
+    fs.readFile('css/style.css', (err, data) => {
+      if (err) {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Not Found');
+        return;
+      }
+      res.writeHead(200, { 'Content-Type': 'text/css' });
+      res.end(data);
+    });
+  } else if (page === '/js/main.js') {
+    fs.readFile('js/main.js', (err, data) => {
+      if (err) {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Not Found');
+        return;
+      }
+      res.writeHead(200, { 'Content-Type': 'text/javascript' });
+      res.end(data);
+    });
+  } else {
+    figlet('404!!', (err, data) => {
+      if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+      }
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end(data);
     });
   }
 });
 
-server.listen(8000||process.env.PORT,()=>{
-  console.log(`The port is listening on port 8000`)
-})
+// Change to use process.env.PORT first, then fallback to 8000
+const PORT = process.env.PORT || 8000;
+server.listen(PORT, () => {
+  console.log(`The server is listening on port ${PORT}`);
+});
