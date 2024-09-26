cc3bc79f82288a8c9c7df59ac5860a96d6b8a12b
aizazhalima1 <halimaaizaz@hotmail.com>
2024-09-26 11:06:08 -0400

    connecting backend

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ const figlet = require('figlet');
 const server = http.createServer((req, res) => {
   const page = url.parse(req.url).pathname;
 
+
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
   if (page === '/') {
     fs.readFile('index.html', (err, data) => {
       if (err) {
@@ -18,18 +23,43 @@ const server = http.createServer((req, res) => {
       res.end(data);
     });
   } else if (page === '/api') {
-   
-    res.setHeader('Access-Control-Allow-Origin', '*'); 
+    // API logic
     res.writeHead(200, { 'Content-Type': 'application/json' });
     const arr = ['Rock', 'Paper', 'Scissor'];
     const rand = Math.floor(Math.random() * arr.length);
     const valComputer = arr[rand];
     const objToJson = { computerVal: valComputer };
     res.end(JSON.stringify(objToJson));
+  } else if (page === '/css/style.css') {
+    fs.readFile('css/style.css', (err, data) => {
+      if (err) {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('CSS Not Found');
+        return;
+      }
+      res.writeHead(200, { 'Content-Type': 'text/css' });
+      res.end(data);
+    });
+  } else if (page === '/js/main.js') {
+    fs.readFile('js/main.js', (err, data) => {
+      if (err) {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('JS Not Found');
+        return;
+      }
+      res.writeHead(200, { 'Content-Type': 'application/javascript' });
+      res.end(data);
+    });
   } else {
- 
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('404 Not Found');
+    figlet('404!!', (err, data) => {
+      if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+      }
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end(data);
+    });
   }
 });
 
